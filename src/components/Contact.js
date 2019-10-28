92494229cfaa3878d02452527d3aa3d0d9762ede
François Boisselier <francois.boisselier@gmail.com>
2019-10-28 10:10:41 +0100

    change the status by clicking on the status itself

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,17 +1,24 @@
 import React from 'react';
 import './styles.css';
 
-function Contact({name, status, avatar}){
-    return(
-        <div className = "Contact">
-            <div><img className="avatar" src = {avatar} alt ={name}/> </div>
-            <div><h4 className='name'> {name}</h4>
-                <div className ='status'> 
-                    <span className={ status ? 'status-online' : 'status-offline'}> </span>
-                    <span className='status-text'> {status ? "online" : "offline"} </span>
-                </div>
-            </div>
-        </div>
-    )
+class Contact extends React.Component{
+        constructor(props){
+            super(props);
+                this.state= {online:false}     
+        }   
+
+                render(){
+                    return(
+                        <div className = "Contact">
+                            <div><img className="avatar" src = {this.props.avatar} alt ={this.props.name}/> </div>
+                            <div><h4 className='name'> {this.props.name}</h4>
+                                <div className ='status' onClick={e => this.state.online ? this.setState({online:false}) : this.setState({online:true})}> 
+                                    <span className={ this.state.online ? 'status-online' : 'status-offline'}> </span>
+                                    <span className='status-text'> {this.state.online ? "online" : "offline"} </span>
+                                </div>
+                            </div>
+                        </div>
+                    )
+        }
 }
 export default Contact
